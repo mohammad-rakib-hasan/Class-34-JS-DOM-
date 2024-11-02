5b10eb660b944687b925219967c81919b0e409a7
mohammad-rakib-hasan <bdprakib9@gmail.com>
2024-11-02 23:46:40 +0600

    JS-DOM

---
+++ main.js
@@ -0,0 +1,23 @@
+function sum (){
+    let x = document.getElementById('number1').value; 
+    let y = document.getElementById('number2').value; 
+
+
+    
+    let result = parseFloat(x) + parseFloat(y); 
+
+document.getElementById('result').innerHTML = 'The result is =' + result;
+}
+
+
+  
+   
+
+
+
+   
+    
+   
+    
+   
+
